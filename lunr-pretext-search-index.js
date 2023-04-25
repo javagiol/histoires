85d7cbabab4503ff842181eae7eeaf09516d0936
javagiol <60525128+javagiol@users.noreply.github.com>
2023-04-25 15:04:07 +0000

    Latest build of target web.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,31 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Pages Liminaires",
+  "number": "",
+  "title": "Pages Liminaires",
+  "body": "   Julien Giol       "
+},
+{
+  "id": "localisation",
+  "level": "1",
+  "url": "localisation.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Localisation",
+  "body": " Localisation  Salut  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})

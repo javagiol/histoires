288d5e7e5f20143681bd2aca8ce21a4061e6b7f0
javagiol <60525128+javagiol@users.noreply.github.com>
2023-04-25 20:46:54 +0000

    Latest build of target web.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -16,14 +16,23 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Localisation",
-  "body": " Localisation  Aux États-Unis, il y a cinquante états dont le plus peuplé est la Californie, avec près de 40 millions d'habitants en 2020.  Carte de la Californie    Comment s'appelle l'océan qui borde la Californie?    L'Océan Pacifique.    La capitale de la Californie n'est pas sa ville la plus grande, ni la plus connue : c'est Sacramento, à mi-chemin entre San Francisco et le lac Tahoe.    Quel est le numéro de l'autoroute inter-États qui relie notamment Sacramento à Los Angeles?    L'Interstate 5.    La ville la plus peuplée et la plus étendue de Californie, c'est Los Angeles. Son nom signifie les anges en espagnol.    Quel est le pays hispanophone qu'on trouve juste au sud de la Californie?    Le Mexique.    Westwood est un quartier de Los Angeles situé au nord-ouest de la ville.  Carte de Westwood    Quel est le nom du boulevard sinueux qui délimite le nord de Westwood?    Sunset Boulevard.    Pendant l'année scolaire 2005-2006, j'ai habité à Westwood chez Alex et Anne-Marie, au 461 Levering Avenue.  461 Levering Avenue    Quel type d'arbre dépasse du toit derrière la maison?    Un palmier.    "
+  "body": " Localisation  Aux États-Unis, il y a cinquante états dont le plus peuplé est la Californie, avec près de 40 millions d'habitants en 2020.   La Californie.      Comment s'appelle l'océan qui borde la Californie?    L'Océan Pacifique.    La capitale de la Californie n'est pas sa ville la plus grande, ni la plus connue : c'est Sacramento, à mi-chemin entre San Francisco et le lac Tahoe.    Quel est le numéro de l'autoroute inter-États qui relie notamment Sacramento à Los Angeles?    L'Interstate 5.    La ville la plus peuplée et la plus étendue de Californie, c'est Los Angeles. Son nom signifie les anges en espagnol.    Quel est le pays hispanophone qu'on trouve juste au sud de la Californie?    Le Mexique.    Westwood est un quartier de Los Angeles situé au nord-ouest de la ville.   Westwood, quartier de Los Angeles      Quel est le nom du boulevard sinueux qui délimite le nord de Westwood?    Sunset Boulevard.    Pendant l'année scolaire 2005-2006, j'ai habité à Westwood chez Alex et Anne-Marie, au 461 Levering Avenue.   461 Levering Avenue.      Quel type d'arbre dépasse du toit derrière la maison?    Un palmier.    "
+},
+{
+  "id": "carte-californie",
+  "level": "2",
+  "url": "localisation.html#carte-californie",
+  "type": "Figure",
+  "number": "1.1.1",
+  "title": "",
+  "body": " La Californie.   "
 },
 {
   "id": "question-1",
   "level": "2",
   "url": "localisation.html#question-1",
   "type": "Question",
-  "number": "1.1.1",
+  "number": "1.1.2",
   "title": "",
   "body": "  Comment s'appelle l'océan qui borde la Californie?    L'Océan Pacifique.   "
 },
@@ -32,7 +41,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "localisation.html#question-2",
   "type": "Question",
-  "number": "1.1.2",
+  "number": "1.1.3",
   "title": "",
   "body": "  Quel est le numéro de l'autoroute inter-États qui relie notamment Sacramento à Los Angeles?    L'Interstate 5.   "
 },
@@ -41,25 +50,43 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "localisation.html#question-3",
   "type": "Question",
-  "number": "1.1.3",
+  "number": "1.1.4",
   "title": "",
   "body": "  Quel est le pays hispanophone qu'on trouve juste au sud de la Californie?    Le Mexique.   "
+},
+{
+  "id": "carte-westwood",
+  "level": "2",
+  "url": "localisation.html#carte-westwood",
+  "type": "Figure",
+  "number": "1.1.5",
+  "title": "",
+  "body": " Westwood, quartier de Los Angeles   "
 },
 {
   "id": "question-4",
   "level": "2",
   "url": "localisation.html#question-4",
   "type": "Question",
-  "number": "1.1.4",
+  "number": "1.1.6",
   "title": "",
   "body": "  Quel est le nom du boulevard sinueux qui délimite le nord de Westwood?    Sunset Boulevard.   "
+},
+{
+  "id": "carte-levering",
+  "level": "2",
+  "url": "localisation.html#carte-levering",
+  "type": "Figure",
+  "number": "1.1.7",
+  "title": "",
+  "body": " 461 Levering Avenue.   "
 },
 {
   "id": "question-5",
   "level": "2",
   "url": "localisation.html#question-5",
   "type": "Question",
-  "number": "1.1.5",
+  "number": "1.1.8",
   "title": "",
   "body": "  Quel type d'arbre dépasse du toit derrière la maison?    Un palmier.   "
 }
